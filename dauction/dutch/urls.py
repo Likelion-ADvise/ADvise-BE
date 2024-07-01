73c65192443b9ab9173622cfe5e3d9b90a24db62
minkyeong <haileyre000b@naver.com>
2024-07-01 13:51:47 +0900

    게시물 조회, 댓글 삭제

--- dauction/dutch/urls.py
+++ dauction/dutch/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import create_ad, create_proposal
+from .views import create_ad, get_ad, create_proposal, delete_proposal
 
 urlpatterns = [
     path('ads/', create_ad, name='create_ad'),
     path('ads/<int:ad_id>/', create_proposal, name='create_proposal'),
+    path('postad/<int:ad_id>/<int:pk>/delete/', delete_proposal, name='delete_proposal'),
+    path('postad/<int:pk>/', get_ad, name='get_ad'),
 ]
