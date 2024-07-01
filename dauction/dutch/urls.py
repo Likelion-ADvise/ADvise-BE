91ff5c9e6f848ec895a8d9070830c4f5e97a258d
wxxwls0422 <dark010422@naver.com>
2024-07-01 14:55:42 +0900

    search

--- dauction/dutch/urls.py
+++ dauction/dutch/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import get_all_ads, search_ads, create_ad, get_ad, create_proposal, delete_proposal
 
 urlpatterns = [
-    path('ads/', create_ad, name='create_ad'),
-    path('ads/<int:ad_id>/', create_proposal, name='create_proposal'),
-    path('ads/<int:ad_id>/<int:pk>/delete/', delete_proposal, name='delete_proposal'),
-    path('ads/<int:pk>/', get_ad, name='get_ad'),
-    path('', get_all_ads, name='get_all_ads'),
+    path('ads/', create_ad, name='create_ad'), #게시글 작성
+    path('ads/<int:ad_id>/', create_proposal, name='create_proposal'), #댓글 작성
+    path('ads/<int:ad_id>/<int:pk>/delete/', delete_proposal, name='delete_proposal'), #댓글 삭제
+    path('ads/<int:pk>/', get_ad, name='get_ad'), #특정 게시물 검색
+    path('', get_all_ads, name='get_all_ads'), # 초기 화면 설정
     path('ads/search/', search_ads, name='search_ads'),  # 제목 검색
     
 ]
