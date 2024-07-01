6c2550c4efbe4c40fd45caf8be68a81f31bf7266
minkyeong <haileyre000b@naver.com>
2024-07-01 11:08:59 +0900

    model 설정, 게시물 api 작성

--- dauction/dutch/urls.py
+++ dauction/dutch/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import create_ad
 
 urlpatterns = [
+    # 광고 게시물 생성
+    path('ads/', create_ad, name='create_ad')
 ]
--- dauction/dutch/views.py
+++ dauction/dutch/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Ad, Proposal
 
-# Create your views here.
+def create_ad(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        title = data.get('title')
+        content = data.get('content')
+        minimum_price = data.get('minimum_price')
+
+        ad = Ad(
+            title=title,
+            content=content,
+            minimum_price=minimum_price
+        )
+        ad.save()
+        return JsonResponse({'message': 'success'})
+    return JsonResponse({'message': 'POST 요청만 허용됩니다.'}, status=400)
